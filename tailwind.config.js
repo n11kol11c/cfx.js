1539403aec4a8a3912d848a0b64fb578b4936d58
м <174154109+n11kol11c@users.noreply.github.com>
2026-04-04 20:04:42 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./ui/**/*.{html,js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
