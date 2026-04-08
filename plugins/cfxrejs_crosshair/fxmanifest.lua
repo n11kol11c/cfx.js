58ec33d3313469aa7685ae0cc63cacc8a10777af
м <174154109+n11kol11c@users.noreply.github.com>
2026-04-08 22:40:43 +0200

    Create fxmanifest.lua

---
+++ plugins/cfxrejs_crosshair/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Matija'
+description 'Custom Crosshair for cfxjs'
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html'
+}
+
+client_scripts {
+    'data/dist/client.js'
+}
